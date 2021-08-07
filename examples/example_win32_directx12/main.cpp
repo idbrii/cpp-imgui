57866ad39e87583ff1d9976046582be1f28339a8
David Briscoe <idbrii@gmail.com>
2021-08-06 23:14:37 -0700

    Use software mouse for better screenshots

--- examples/example_win32_directx12/main.cpp
+++ examples/example_win32_directx12/main.cpp
@@ -85,6 +85,7 @@ int main(int, char**)
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.MouseDrawCursor = true;
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
